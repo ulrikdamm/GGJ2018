75ffa05b4cb52cf2da3d937dbf3e2b6c41d05fb9
jmapostigo <35850092+jmapostigo@users.noreply.github.com>
2018-01-27 10:52:58 +0100

    Player controller, Gestor, barra de energia,

---
+++ Assets/Script/Energia.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Energia : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        { 
+        Destroy(gameObject);
+        Gestor.gestor.player001.value += 1;
+        }
+    }
+}

b053ce0ae99dfddd9ffe9a462baf9d81eb2808f6
Ulrik Damm <ulrikdamm@me.com>
2018-01-27 22:33:42 +0100

    Menu

---
+++ Assets/EstadoJuego.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using UnityEngine.SceneManagement;
+
+public class EstadoJuego : MonoBehaviour {
+
+    public static EstadoJuego estadoJuego;
+
+    void Awake()
+    {
+        if (estadoJuego == null)
+        {
+            estadoJuego = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (estadoJuego != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnLevelWasLoaded(int level)
+    {
+        if (level == 2)
+        { 
+        Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}

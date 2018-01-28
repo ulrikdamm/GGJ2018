efb8f20008329f377d01a99b0d3607d1b939fff7
Ulrik Damm <ulrikdamm@me.com>
2018-01-28 12:27:34 +0100

    Area de pilas

--- Assets/BatteryDropArea.cs
+++ Assets/BatteryDropArea.cs
@@ -1,19 +1,47 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class BatteryDropArea : MonoBehaviour {
 	[SerializeField] Robot robot;
+	[SerializeField] SpriteRenderer spriteRenderer;
 	int chargeCount;
+	
+	[SerializeField] Sprite zeroChargesSprite;
+	[SerializeField] Sprite oneChargeSprite;
+	[SerializeField] Sprite twoChargesSprite;
+	[SerializeField] Sprite threeChargesSprite;
 	
 	public void addCharge() {
 		chargeCount += 1;
 		
 		if (chargeCount >= 3) {
 			robot.becomeActive();
-			chargeCount = 0;
+			StartCoroutine(resetChargesRoutine());
 		} else if (chargeCount == 2) {
 			robot.becomeCharged(false);
 		} else {
 			robot.becomeCharged(true);
 		}
+		
+		updateChargeCountDisplay();
+	}
+	
+	IEnumerator resetChargesRoutine() {
+		yield return new WaitForSeconds(4);
+		chargeCount = 0;
+		updateChargeCountDisplay();
+	}
+	
+	void updateChargeCountDisplay() {
+		switch (chargeCount) {
+			case 0: spriteRenderer.sprite = zeroChargesSprite; break;
+			case 1: spriteRenderer.sprite = oneChargeSprite; break;
+			case 2: spriteRenderer.sprite = twoChargesSprite; break;
+			case 3: default: spriteRenderer.sprite = threeChargesSprite; break;
+		}
+	}
+	
+	void OnValidate() {
+		if (spriteRenderer == null) { spriteRenderer = GetComponent<SpriteRenderer>(); }
 	}
 }
